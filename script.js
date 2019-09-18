946a245429d4187b9a1f5f26c8ae5c58ce18f75d
mohamadD <rediab88@gmail.com>
2019-09-18 17:59:41 +0300

    add calcrout function

--- script.js
+++ script.js
@@ -13,22 +13,77 @@ var map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
 //create a DirectionService object
 //to use the route method and get a result for our request
 
+var directionsService = new google.maps.DirectionsService();
+
 //create a DirectionsRendere object which we will
 //use to display the rout
+var directionsDisplay = new google.maps.DirectionsRenderer();
+
 
 //bind the DirectionsRendere to the map
+directionsDisplay.setMap(map);
 
-//define calcRout function
-function calcRout() {
+
+var from =document.getElementById('from').value;
+var to =document.getElementById('to').value;
+
+
+//define calcRoute function
+function calcRoute() {
   //create request
+ 
+ var request={
+     origin: from,
+     destination: to,
+     travelMode: google.maps.TravelMode.DRIVING, //WALKING, BYCYCLING, TRANSIT
+    unitsystem: google.maps.UnitSystem.IMPERIAL
+
+   }
+
+
+
   //pass the request to the route method
-  //if status ok
-  //Get distance and time
-  //display route using
-  //DirectionRenderer object
-  //if status not ok
-  //delete toute from map
-  //center map in london
+  directionsService.route(request, function(result,status){
+    //if status ok
+    if(status== google.maps.DirectionStatus.OK){
+
+   //Get distance and time
+      document.getElementById('output').innerHTML= 
+      '<div class="">From'+from +
+      ":<br> To"+to+":<br> Driving distination"+
+       result.routes[0].legs[0].distance.text +
+       "<br> Duration:"+ result[0].routes[0].duration.text+"</div>";
+
+           //display route 
+      directionsDisplay.setDirections(result);
+
+
+    }else{
+       //delete toute from map
+
+       directionsDisplay.setDirections({routes:[]});
+
+
+         //center map in natzareth
+         map.setCenter(myLatLng);
+
+
+         //show error message
+         document.getElementById('output').innerHTML='<div class=""> could not retrieve distance </div>'  ;
+    }
+  });
+
 }
 
 //create autocomplete object for all inputs
+
+var options={
+  types:['(cities)']
+};
+
+
+ from =document.getElementById('from');
+ to =document.getElementById('to');
+
+var autucomplete1= new google.maps.places.Autocomplete(from,options);
+var autucomplete2= new google.maps.places.Autocomplete(to,options);
