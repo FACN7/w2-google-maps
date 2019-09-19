d7c8bf7eeb1d5ca96695723caf23597f44442867
mohamadD <rediab88@gmail.com>
2019-09-19 10:35:42 +0300

    last version without updated readme

--- script.js
+++ script.js
@@ -1,89 +1,69 @@
-//set map option
-
-var myLatLng = { lat: 32.699635, lng: 35.303547 };
+//javascript.js
+//set map options
+var myLatLng = {lat: 32.794044, lng: 34.989571};
 var mapOptions = {
-  center: myLatLng,
-  zoom: 13,
-  mapTypeId: google.maps.MapTypeId.ROADMAP
+    center: myLatLng,
+    zoom: 12,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+    
 };
 
 //create map
-var map = new google.maps.Map(document.getElementById("googleMap"), mapOptions);
+var map = new google.maps.Map(document.getElementById('googleMap'), mapOptions);
 
-//create a DirectionService object
-//to use the route method and get a result for our request
-
+//create a DirectionsService object to use the route method and get a result for our request
 var directionsService = new google.maps.DirectionsService();
 
-//create a DirectionsRendere object which we will
-//use to display the rout
+//create a DirectionsRenderer object which we will use to display the route
 var directionsDisplay = new google.maps.DirectionsRenderer();
 
-
-//bind the DirectionsRendere to the map
+//bind the DirectionsRenderer to the map
 directionsDisplay.setMap(map);
 
 
-var from =document.getElementById('from').value;
-var to =document.getElementById('to').value;
-
-
 //define calcRoute function
-function calcRoute() {
-  //create request
- 
- var request={
-     origin: from,
-     destination: to,
-     travelMode: google.maps.TravelMode.DRIVING, //WALKING, BYCYCLING, TRANSIT
-    unitsystem: google.maps.UnitSystem.IMPERIAL
-
-   }
+var from=document.getElementById("from");
+var to=document.getElementById("to");
 
 
 
-  //pass the request to the route method
-  directionsService.route(request, function(result,status){
-    //if status ok
-    if(status== google.maps.DirectionStatus.OK){
+function calcRoute(){
+//create request
+var request = {
+origin: from.value,
+destination:to.value,
+travelMode: google.maps.TravelMode.DRIVING, //WALKING, BYCYCLING, TRANSIT
+unitSystem: google.maps.UnitSystem.METRIC
+}
 
-   //Get distance and time
-      document.getElementById('output').innerHTML= 
-      '<div class="">From'+from +
-      ":<br> To"+to+":<br> Driving distination"+
-       result.routes[0].legs[0].distance.text +
-       "<br> Duration:"+ result[0].routes[0].duration.text+"</div>";
+//pass the request to the route method
+directionsService.route(request, function(result, status){
+if(status == google.maps.DirectionsStatus.OK){
 
-           //display route 
-      directionsDisplay.setDirections(result);
+//Get distance and time
+document.getElementById("output").innerHTML="<div class='alert-info'>From: "+from.value+".<br />To: "+to.value+".<br /> Driving distance: "+result.routes[0].legs[0].distance.text+".<br />Duration: "+result.routes[0].legs[0].duration.text+".</div>";
 
+//display route
+directionsDisplay.setDirections(result);
+}else{
+//delete route from map
+directionsDisplay.setDirections({routes: []});
+//center map in London
+map.setCenter(myLatLng);
 
-    }else{
-       //delete toute from map
-
-       directionsDisplay.setDirections({routes:[]});
-
-
-         //center map in natzareth
-         map.setCenter(myLatLng);
-
-
-         //show error message
-         document.getElementById('output').innerHTML='<div class=""> could not retrieve distance </div>'  ;
-    }
-  });
+//show error message
+document.getElementById("output").innerHTML="<div class='alert-danger'>Could not retrieve driving distance.</div>";
+}
+});
 
 }
 
-//create autocomplete object for all inputs
-
-var options={
-  types:['(cities)']
-};
 
 
- from =document.getElementById('from');
- to =document.getElementById('to');
 
-var autucomplete1= new google.maps.places.Autocomplete(from,options);
-var autucomplete2= new google.maps.places.Autocomplete(to,options);
+//create autocomplete objects for all inputs
+var options = {
+types: ['(cities)']   
+}
+var autocomplete1 = new google.maps.places.Autocomplete(from, options);
+var autocomplete2 = new google.maps.places.Autocomplete(to, options);
